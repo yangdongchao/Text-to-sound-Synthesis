c5eed70837fbc9e77d5969f50447839d8d45bb90
yangdongchao <15087581161@163.com>
2022-08-03 16:10:24 +0800

    666

--- Codebook/start/start.sh
+++ Codebook/start/start.sh
@@ -1,14 +1,14 @@
 source activate specvqgan
-# python3 /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/train.py \ 
-#         --base  /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/configs/caps_transformer_small.yaml -t True --gpus 0,
+# python3 code3/SpecVQGAN/train.py \ 
+#         --base  code3/SpecVQGAN/configs/caps_transformer_small.yaml -t True --gpus 0,
 
-python3 /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/train.py --base /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/configs/audioset_codebook128.yaml -t True --gpus 0,1,2,3,4,5,6,7,
-# /root/anaconda3/envs/specvqgan/bin/python /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/train.py --base /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/configs/caps_transformer.yaml -t True --gpus 0,1,2,3,4,5,6,7, \
-#         model.params.first_stage_config.params.ckpt_path=/apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/logs/2022-04-24T23-17-27_audioset_codebook256/checkpoints/last.ckpt
+python3 code3/SpecVQGAN/train.py --base code3/SpecVQGAN/configs/audioset_codebook128.yaml -t True --gpus 0,1,2,3,4,5,6,7,
+# /root/anaconda3/envs/specvqgan/bin/python code3/SpecVQGAN/train.py --base code3/SpecVQGAN/configs/caps_transformer.yaml -t True --gpus 0,1,2,3,4,5,6,7, \
+#         model.params.first_stage_config.params.ckpt_path=code3/SpecVQGAN/logs/2022-04-24T23-17-27_audioset_codebook256/checkpoints/last.ckpt
 
-# EXPERIMENT_PATH="/apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/logs/2022-05-05T19-28-48_caps_transformer"
-# SPEC_DIR_PATH="/apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/data/audiocaps/features/*/melspec_10s_22050hz/"
-# cls_token_dir_path="/apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/data/audiocaps/clip_text/*/cls_token_512/"
+# EXPERIMENT_PATH="code3/SpecVQGAN/logs/2022-05-05T19-28-48_caps_transformer"
+# SPEC_DIR_PATH=code3/SpecVQGAN/data/audiocaps/features/*/melspec_10s_22050hz/"
+# cls_token_dir_path="/code3/SpecVQGAN/data/audiocaps/clip_text/*/cls_token_512/"
 # SAMPLES_FOLDER="caps_validation"
 # # SPLITS="\"[validation, ]\""
 # SPLITS="[validation]"
@@ -25,8 +25,8 @@ python3 /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/train.py --base /ap
 #     --master_addr=localhost \
 #     --master_port=62374 \
 #     --use_env \
-#         /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/evaluation/generate_samples_caps.py \
-#         sampler.config_sampler=/apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/evaluation/configs/sampler.yaml \
+#         code3/SpecVQGAN/evaluation/generate_samples_caps.py \
+#         sampler.config_sampler=code3/SpecVQGAN/evaluation/configs/sampler.yaml \
 #         sampler.model_logdir=$EXPERIMENT_PATH \
 #         sampler.splits=$SPLITS \
 #         sampler.samples_per_video=$SAMPLES_PER_VIDEO \
@@ -37,8 +37,8 @@ python3 /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/train.py --base /ap
 #         data.params.cls_token_dir_path=$cls_token_dir_path \
 #         sampler.now=$NOW
 
-# python /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/evaluation/generate_samples_caps.py \
-#         sampler.config_sampler=/apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/evaluation/configs/sampler.yaml \
+# python code3/SpecVQGAN/evaluation/generate_samples_caps.py \
+#         sampler.config_sampler=code3/SpecVQGAN/evaluation/configs/sampler.yaml \
 #         sampler.model_logdir=$EXPERIMENT_PATH \
 #         sampler.splits=$SPLITS \
 #         sampler.samples_per_video=$SAMPLES_PER_VIDEO \
@@ -49,9 +49,9 @@ python3 /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/train.py --base /ap
 #         data.params.cls_token_dir_path=$cls_token_dir_path \
 #         sampler.now=$NOW
 
-# ROOT='/apdcephfs/share_1316500/donchaoyang/code3/VQ-Diffusion/OUTPUT/caps_train/2022-05-08T19-56-36/fast_inf5_samples_2022-05-15T16-33-50'
-# python /apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/evaluate.py \
-#         config=/apdcephfs/share_1316500/donchaoyang/code3/SpecVQGAN/evaluation/configs/eval_melception_${DATASET,,}.yaml \
+# ROOT='OUTPUT/caps_train/2022-05-08T19-56-36/fast_inf5_samples_2022-05-15T16-33-50'
+# python code3/SpecVQGAN/evaluate.py \
+#         config=code3/SpecVQGAN/evaluation/configs/eval_melception_${DATASET,,}.yaml \
 #         input2.path_to_exp=$EXPERIMENT_PATH \
 #         patch.specs_dir=$SPEC_DIR_PATH \
 #         patch.spec_dir_path=$SPEC_DIR_PATH \
